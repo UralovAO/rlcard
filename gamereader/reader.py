60eb4f2873c6b101cac0351909d1d7bb1f5f24d6
UralovAO <UralovAO@sibgenco.ru>
2021-05-31 18:44:37 +0700

    some changes

--- gamereader/reader.py
+++ gamereader/reader.py
@@ -11,6 +11,7 @@ from ocr.cards import ocr_cards
 from ocr.digits import ocr_digits
 import cv2
 
+BIG_BLIND = 100
 HERE = path.abspath(path.dirname(__file__))
 # screenshots_path = path.join(HERE, 'screenshots')
 
@@ -28,31 +29,42 @@ class Screen(object):
         top = 0
         game_window.moveTo(left, top)
         game_window.size = (width, hight)
+        time.sleep(0.5)
 
     def get_button_position(self):
-        DELTA = 5
-        regions = {0: (1006 - DELTA, 655 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
-                   1: (709 - DELTA, 495 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
-                   2: (732 - DELTA, 363 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
-                   3: (1214 - DELTA, 270 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
-                   4: (1578 - DELTA, 355 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
-                   5: (1504 - DELTA, 601 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA)}
-        for player_id in regions.keys():
-            button = pyautogui.locateOnScreen(path.join(HERE, 'data', 'signs', 'button.png'),
-                                              confidence=0.90,
-                                              region=regions[player_id],
-                                              grayscale=True)
-            # print('Button with coordinates =', button, 'is found')
-            if button is not None:
-                return player_id
+        def get_button_position_from_screen():
+            DELTA = 5
+            regions = {0: (1006 - DELTA, 655 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
+                       1: (709 - DELTA, 495 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
+                       2: (732 - DELTA, 363 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
+                       3: (1214 - DELTA, 270 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
+                       4: (1578 - DELTA, 355 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA),
+                       5: (1504 - DELTA, 601 - DELTA, 50 + 2 * DELTA, 41 + 2 * DELTA)}
+            for player_id in regions.keys():
+                button = pyautogui.locateOnScreen(path.join(HERE, 'data', 'signs', 'button.png'),
+                                                  confidence=0.90,
+                                                  region=regions[player_id],
+                                                  grayscale=True)
+                # print('Button with coordinates =', button, 'is found')
+                if button is not None:
+                    return player_id
 
-        if button is None:
+        button_position = get_button_position_from_screen()
+        if button_position is None:
+            # lets try again later
+            time.sleep(1)
+            button_position = get_button_position_from_screen()
+
+        if button_position is None:
             sys.exit('Application did not find buttion position!')
+        else:
+            return button_position
 
     def get_bets(self):
         DELTA = 0
         bets = {}
-        regions = {1: (805 - DELTA, 568 - DELTA, 270 + 2 * DELTA, 23 + 2 * DELTA),
+        regions = {0: (1110 - DELTA, 629 - DELTA, 270 + 2 * DELTA, 23 + 2 * DELTA),
+                   1: (805 - DELTA, 568 - DELTA, 270 + 2 * DELTA, 23 + 2 * DELTA),
                    2: (825 - DELTA, 349 - DELTA, 270 + 2 * DELTA, 23 + 2 * DELTA),
                    3: (1240 - DELTA, 250 - DELTA, 270 + 2 * DELTA, 23 + 2 * DELTA),
                    4: (1230 - DELTA, 313 - DELTA, 270 + 2 * DELTA, 23 + 2 * DELTA),
@@ -151,8 +163,30 @@ class Screen(object):
         return folded_players
 
 class Game(object):
-    # def __init__(self):
-    #     self.current_stage
+    def __init__(self):
+        self.button_position = None
+        self.bets = None
+        self.folded_players = None
+        self.decisions_after_BB = None
+        self.player_cards = None
+        self.pot = None
+        self.flop_cards = None
+        self.turn_card = None
+        self.river_card = None
+        self.decisions_before_BB = None
+        self.current_street = None
+        self.first_round_sign = True
+
+    def start(self):
+        self.pot = 0
+        self.set_first_round_sign(True)
+
+    def set_first_round_sign(self, first_round_sign):
+        self.first_round_sign = first_round_sign
+
+    def get_first_round_sign(self):
+        return self.first_round_sign
+
     def set_button_position(self):
         screen = Screen()
         self.button_position = screen.get_button_position()
@@ -170,8 +204,11 @@ class Game(object):
     def get_bets(self):
         return self.bets
 
-    def set_pot(self):
-        self.pot = sum(filter(lambda x: x is not None, self.bets.values()))
+    def update_pot(self):
+        if self.pot is not None:
+            self.pot = self.pot + sum(filter(lambda x: x is not None, self.bets.values()))
+        else:
+            sys.exit('Pot firstly must be inited with method game.start()')
 
     def get_pot(self):
         return self.pot
@@ -260,86 +297,99 @@ class Game(object):
     def get_folded_players(self):
         return self.folded_players
 
-    def set_current_decisions(self):
+    def set_decisions_after_BB(self):
+        print('start set_decisions_after_BB')
 
         button_position = self.get_button_position()
         bets = self.get_bets()
         pot = self.get_pot()
         folded_players = self.get_folded_players()
-        self.current_decisions = {}
+        self.decisions_after_BB = {}
 
         previous_bet = BIG_BLIND
         # is_previous_allin = False
 
+        print('button_position = ',button_position)
+        print('bets = ', bets)
+        print('pot = ', pot)
+        print('folded_players = ', folded_players)
+
         # we need decisions only after BB position and before ours
         # this decisions we pass to AI
-        for player_id in range(button_position+2, 6):
+        start_position = (button_position+3)%6
+        if start_position == 0:
+            return
+
+        for player_id in range(start_position, 6): # if buuton position+3 is more then 6 then we have to make it less then 6
+            print('player_id = ', player_id)
+            print('previous_bet = ', previous_bet)
+            pot = self.get_pot(player_id)
             if player_id in folded_players:
-                self.current_decisions[player_id] = 'FOLD'
+                self.decisions_after_BB[player_id] = 'FOLD'
             elif bets[player_id] is None:
-                self.current_decisions[player_id] = 'CHECK'
-            elif 'ALL_IN' in list(self.current_decisions.values()):
+                self.decisions_after_BB[player_id] = 'CHECK'
+            elif 'ALL_IN' in list(self.decisions_after_BB.values()):
             # elif is_previous_allin  == True:
-                self.current_decisions[player_id] = 'CALL'
+                self.decisions_after_BB[player_id] = 'CALL'
             elif bets[player_id] == previous_bet:
-                self.current_decisions[player_id] = 'CALL'
+                self.decisions_after_BB[player_id] = 'CALL'
             elif bets[player_id] > previous_bet and bets[player_id] <= (3 * pot / 4):
-                self.current_decisions[player_id] = 'RAISE_HALF_POT'
+                self.decisions_after_BB[player_id] = 'RAISE_HALF_POT'
                 previous_bet = bets[player_id]
             elif bets[player_id] > (3 * pot / 4) and bets[player_id] <= pot + (100 * BIG_BLIND - pot) / 2:
-                self.current_decisions[player_id] = 'RAISE_POT'
+                self.decisions_after_BB[player_id] = 'RAISE_POT'
                 previous_bet = bets[player_id]
             elif bets[player_id] > pot + (100 * BIG_BLIND - pot) / 2:
-                self.current_decisions[player_id] = 'ALL_IN'
+                self.decisions_after_BB[player_id] = 'ALL_IN'
                 previous_bet = bets[player_id]
                 # is_previous_allin = True
             else:
-                sys.exit('Error. Application can not define decision!')
-
-        return current_decisions
+                sys.exit('Error. Application can not define decision after big blind!')
+        print('finish set_decisions_after_BB')
+        print(' ')
         pass
 
-    def get_current_decisions(self):
-        return self.current_decisions
+    def get_decisions_after_BB(self):
+        return self.decisions_after_BB
 
-    def set_previous_decisions(self):
+    def set_decisions_before_BB(self):
 
         button_position = self.get_button_position()
         bets = self.get_bets()
         pot = self.get_pot()
         folded_players = self.get_folded_players()
 
-        self.previous_decisions = self.current_decisions
+        self.decisions_before_BB = self.decisions_after_BB
 
-        previous_bet = self.previous_decisions[0] # get player's (our) bet
+        previous_bet = self.bets[0] # get player's (our) bet
         is_previous_allin = False
 
         for player_id in range(1, button_position+2):
             if player_id in folded_players:
-                self.previous_decisions[player_id] = 'FOLD'
-            elif 'ALL_IN' in list(self.previous_decisions.values()):
-                self.previous_decisions[player_id] = 'CALL'
+                self.decisions_before_BB[player_id] = 'FOLD'
+            elif 'ALL_IN' in list(self.decisions_before_BB.values()):
+                self.decisions_before_BB[player_id] = 'CALL'
             elif player_id == button_position + 2 and bets[player_id] == BIG_BLIND: # check on BB position
-                self.previous_decisions[player_id] = 'CHECK'
+                self.decisions_before_BB[player_id] = 'CHECK'
             # elif is_previous_allin  == True:
             #     self.current_decisions[player_id] = 'CALL'
             elif bets[player_id] == previous_bet:
-                self.previous_decisions[player_id] = 'CALL'
+                self.decisions_before_BB[player_id] = 'CALL'
             elif bets[player_id] > previous_bet and bets[player_id] <= (3 * pot / 4):
-                self.previous_decisions[player_id] = 'RAISE_HALF_POT'
+                self.decisions_before_BB[player_id] = 'RAISE_HALF_POT'
                 previous_bet = bets[player_id]
             elif bets[player_id] > (3 * pot / 4) and bets[player_id] <= pot + (100 * BIG_BLIND - pot) / 2:
-                self.previous_decisions[player_id] = 'RAISE_POT'
+                self.decisions_before_BB[player_id] = 'RAISE_POT'
                 previous_bet = bets[player_id]
             elif bets[player_id] > pot + 3*(100 * BIG_BLIND - pot) / 4:
-                self.previous_decisions[player_id] = 'ALL_IN'
+                self.decisions_before_BB[player_id] = 'ALL_IN'
                 previous_bet = bets[player_id]
                 is_previous_allin = True
             else:
-                sys.exit('Error. Application can not define decision!')
+                sys.exit('Error. Application can not define decision before big blind!')
 
-    def get_previous_decisions(self):
-        return self.previous_decisions
+    def get_decisions_before_BB(self):
+        return self.decisions_before_BB
 
 # class RoundState(object):
 #     button_position
@@ -352,16 +402,21 @@ class Game(object):
 
 
 if __name__ == '__main__':
-    time.sleep(4)
+    time.sleep(6)
 
     screen = Screen()
     screen.set_work_position()
 
     game = Game()
 
+###########
+
+    # screen = pyautogui.screenshot(path.join(HERE, 'data', 'screenshots', 'screenshot.png'), region=(1110, 629, 270, 23))
+    # sys.exit('hh')
+##########
     # print('folded = ', screen.get_folded_players())
     # sys.exit('g')
-    is_first_round = True
+    # is_first_round = True
     previous_street = None
 
     while True:
@@ -374,118 +429,62 @@ if __name__ == '__main__':
             game.set_button_position()
             game.set_bets()
             game.set_folded_players()
+
             # game.set_previous_decisions()
             # game.set_current_decisions()
 
             street = game.get_current_street()
-            if street == previous_street:
-                is_first_round = False
 
-            if is_first_round:
+            # if street == previous_street:
+            #     is_first_round = False
+            # else:
+            #     is_first_round = True
+            #
+            # previous_street = street
+
+            if street == 'PREFLOP' and previous_street != 'PREFLOP':
+                print('GAME IS STARTED!')
+                game.start()
+            else:
+                game.set_first_round_sign(False)
+
+            print(street)
+
+            game.update_pot()
+
+            if game.get_first_round_sign():
                 game.set_decisions_after_BB()
+                print('first_round')
+                print('decisions_after_BB = ', game.get_decisions_after_BB())
             else:
                 game.set_decisions_before_BB()
                 game.set_decisions_after_BB()
+                print('not_first_round')
+                print('decisions_before_BB = ', game.get_decisions_before_BB())
+                print('decisions_after_BB = ', game.get_decisions_after_BB())
 
-            print(street)
-            print('button = ', game.get_button_position())
-            if street == 'PREFLOP':
-                print('bets = ', game.get_bets())
-                print('player_cards = ', game.get_player_cards())
+
+            # print('button = ', game.get_button_position())
+            # if street == 'PREFLOP':
+            #     print('bets = ', game.get_bets())
+            #     print('player_cards = ', game.get_player_cards())
                 # print('community_cards = ', preflop.get_community_cards())
-            elif street == 'FLOP':
-                print('bets = ', game.get_bets())
+            # elif street == 'FLOP':
+            #     print('bets = ', game.get_bets())
                 # print('player_cards = ', flop.get_player_cards())
-                print('flop_cards = ', game.get_flop_cards())
-            elif street == 'TURN':
-                print('bets = ', game.get_bets())
-                print('turn_cards = ', game.get_turn_card())
-            elif street == 'RIVER':
-                print('bets = ', game.get_bets())
+                # print('flop_cards = ', game.get_flop_cards())
+            # elif street == 'TURN':
+            #     print('bets = ', game.get_bets())
+            #     print('turn_cards = ', game.get_turn_card())
+            # elif street == 'RIVER':
+            #     print('bets = ', game.get_bets())
                 # print('player_cards = ', flop.get_player_cards())
-                print('river_cards = ', game.get_river_card())
+                # print('river_cards = ', game.get_river_card())
 
-            previous_street = street
+            # previous_street = street
 
             # we gave data to model
             # model gave us back answer
             if game.wait_for_player_bet():
                 pass
-
-        # if stage == 'PREFLOP':
-
-
-
-
-
-
-
-    # while True:
-    #     time.sleep(1)
-    # preflop = Preflop()
-    # if preflop.is_started():
-    #     print('GAME IS STARTED!')
-    #     print('button position = ', game.get_button_position())
-    #     print('bets = ', preflop.get_bets())
-    #     print('player_cards = ', preflop.get_player_cards())
-    #
-    #     if preflop.bet_is_placed():
-    #         stage = game.get_current_stage()
-    #         if stage == 'PREFLOP':
-    #
-    #
-    #
-    #         if preflop.is_finished():
-    #
-    #
-    #
-    #         flop = Flop()
-    #         if flop.is_started():
-    #
-    #         break
-
-######
-
-        # pictures_path = sorted(glob(path.join(r'D:\Development\PyCharm\rlcard\ocr\cards\data\pictures', '*.png')))
-        # print('pictures_path = ', pictures_path)
-
-        # screen = pyautogui.screenshot(path.join(screenshots_path, 'screenshot.png'),
-        #                               region=(970, 220, 450, 70))
-
-        # pyautogui.moveTo(50, 100)
-        # for picture_path in pictures_path:
-        #     print(picture_path)
-        #     picture = pyautogui.locateOnScreen(picture_path,
-        #                                        confidence=0.90,
-        #                                        region=(900, 370, 100, 150),
-        #                                        grayscale=False)
-        #
-        #     if picture is not None:
-        #         print('picture = ',picture)
-        #         print(f'{datetime.now()} picture_is_found ')
-                # (left=940, top=382, width=24, height=48)
-                # (left=1036, top=382, width=24, height=48)
-                # (left=1132, top=382, width=23, height=48)
-                # (left=1228, top=382, width=24, height=48)
-                # (left=1323, top=382, width=24, height=48)
-
-                # (left=1177, top=685, width=24, height=48)
-                # (left=1087, top=686, width=24, height=48)
-
-                # break
-
-
-            ######
-            # break
-        # break
-        # get_digits()
-        # get_initial_data()
-        # if is_round_started():
-        #     print('ROUND IS STARTED!')
-        # screen = pyautogui.screenshot(path.join(screenshots_path, 'screenshot.png'),
-        #                               region=(970, 220, 450, 70))
-
-        # break
-
-
 
